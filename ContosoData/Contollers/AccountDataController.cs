2d952eb34259d32daacda819ede91b4d60a397cb
Arthur Apanovics <arthur.apanovics@gmail.com>
2017-09-01 16:14:09 +1200

    Data controller: switch to singleton pattern.

--- ContosoData/Contollers/AccountDataController.cs
+++ ContosoData/Contollers/AccountDataController.cs
@@ -7,20 +7,35 @@ using ContosoData.Model;
 
 namespace ContosoData.Contollers
 {
-    public class AccountDataController
+    [Serializable]
+    public static class AccountDataController
     {
-        private ContosoDataContext _context;
-        public AccountDataController()
+        public static IEnumerable<Account> Accounts
+        {
+            get
+            {
+                //TODO: Detect change and only update after changes have been made.
+                // fetch account info on each get
+                Accounts = GetAccounts();
+                return _accounts;
+            }
+            set { _accounts = value; }
+        }
+
+        private static ContosoDataContext _context;
+        private static IEnumerable<Account> _accounts;
+
+        static AccountDataController()
         {
             _context = new ContosoDataContext();
         }
-        public IEnumerable<Account> GetAccounts()
+        private static IEnumerable<Account> GetAccounts()
         {
             return _context.Accounts;
         }
 
         //TODO: Change account from int back to Account
-        public IEnumerable<Transaction> GetTransactionRangeByDate(Account account, DateTime dateStart, DateTime dateEnd)
+        public static IEnumerable<Transaction> GetTransactionRangeByDate(Account account, DateTime dateStart, DateTime dateEnd)
         {
             return _context.Transactions.Where(t =>
                 t.DateTime >= dateStart &&
