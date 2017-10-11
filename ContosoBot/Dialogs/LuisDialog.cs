ebc5e141e83792ccd3ead263fbb037af4a660090
Arthur Apanovics <arthur.apanovics@gmail.com>
2017-10-11 16:09:07 +1300

    Help intent modification

--- ContosoBot/Dialogs/LuisDialog.cs
+++ ContosoBot/Dialogs/LuisDialog.cs
@@ -29,7 +29,24 @@ namespace ContosoBot.Dialogs
         [LuisIntent("Help")]
         public async Task Help(IDialogContext context, LuisResult result)
         {
-            context.Call(new HelpDialog(), Callback);
+            var helpMessage = "Here's what I can help you with:  \n" +
+                                 "* Checking account information  \n" +
+                                 "* Viewing transaction history  \n" +
+                                 "* Performing internal fund transfers  \n" +
+                                 "* Converting and viewing currency rates  \n" +
+                                 "\nExample questions you can try:\n\n" +
+                                 "* What is my account balance?  \n" +
+                                 "* Show me the last transaction to Juniper Networks from 15th April 2017 to 01/06/2017, that is over two hundred dollars  \n" +
+                                 "* Transfer 250$ to my savings account \n" +
+                                 "* Show me the rate of Russian ruble and South Korean won  \n" +
+                                 "Don't forget that you can use 'quit' to exit from a dialog  \n" +
+                                 "Even though my heart is made of transistors, you can still talk to me like you would to a human";
+
+            await context.PostAsync(helpMessage);
+
+            await PostSuggestions(context, "OK, with the instructions out of the way, let's do something cool!");
+
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("Greeting")]
